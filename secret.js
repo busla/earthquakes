8c01466e88f717a13a2658055fc31c6b8524f9f1
Jón Levy <nonni@nonni.cc>
2015-10-14 11:14:56 +0000

    init

---
+++ secret.js
@@ -0,0 +1,8 @@
+module.exports = {
+    USER: process.env.CARTODB_USER,
+    password: process.env.CARTODB_PASSWORD,
+    API_KEY: process.env.CARTODB_API_KEY,
+    CONSUMER_KEY: process.env.CARTODB_CONSUMER_KEY, 
+    CONSUMER_SECRET: process.env.CARTODB_CONSUMER_SECRET,
+};
+
---
+++ server.js
@@ -0,0 +1,66 @@
+var cheerio = require('cheerio');
+var _ = require('underscore');
+var moment = require('moment');
+var request = require('request');
+var CartoDB = require('cartodb');
+var secret = require('./secret.js');
+var schedule = require('node-schedule');
+var client = new CartoDB({user: secret.USER,api_key: secret.API_KEY});
+
+client.on('connect', function() {
+  var splitted;
+  var code;
+  var valuesArr = [];
+  var sqlAll = "select t from {table}";
+  var sql = "insert into {table} (t, the_geom, a, dep, s, q, dl, dd, dr) values ";
+  var url = "http://www.vedur.is/skjalftar-og-eldgos/jardskjalftar/#view=table";
+
+  client.query(sqlAll, {table: 'earthquakes'}, function(err, data){            
+    var rows = [];
+    _.each(data.rows, function(obj){ rows.push(moment.utc(obj.t).toISOString()); });
+    console.log(rows);
+    request(url, function (error, response, html) {
+      if (!error && response.statusCode == 200) {
+        $ = cheerio.load(html);
+        $('script').each(function(i, elem) {
+          if (i == 5) {
+            splitted = $(elem).text().split("\n");
+            code = eval(splitted[10].split('=')[1]);            
+            code.map(function(quakes) {
+              if (!(_.contains(rows, moment.utc(quakes.t).toISOString())))  {             
+                var day = moment.utc(quakes.t).toISOString();
+                quakes.lon = quakes.lon.toString().replace(',','.');
+                quakes.lat = quakes.lat.toString().replace(',','.');
+                quakes.dep = quakes.dep.toString().replace(',','.');
+                quakes.s = quakes.s.toString().replace(',','.');
+                quakes.q = quakes.q.toString().replace(',','.');              
+                quakes.dL = quakes.dL.toString().replace(',','.');              
+
+                sql += "('" + 
+                  day + "'," +
+                  "(ST_SetSRID(ST_Point("+quakes.lon+","+quakes.lat+"),4326))" + "," +
+                  quakes.a + "," +
+                  quakes.dep + "," +
+                  quakes.s + "," +
+                  quakes.q + "," +
+                  quakes.dL + ",'" +
+                  quakes.dD + "','" +
+                  quakes.dR +"'" +
+                   "),";                
+              }
+            })
+          }
+        });
+        sql = sql.substr(0,sql.length-1);
+        client.query(sql, {table: 'earthquakes'}, function(err, data){
+          console.log(data);
+        })        
+      }            
+    });    
+  });  
+
+});
+var j = schedule.scheduleJob('*/1 * * * *', function(){
+    console.log('Running crawler at: ', moment().toISOString());
+    client.connect();
+});
